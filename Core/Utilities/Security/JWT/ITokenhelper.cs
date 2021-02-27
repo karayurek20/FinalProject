a6d387473d9fc6b896cb1b766ccf60ed3dff2b88
Mehmet Can KARAYÜREK <takma_66@hotmail.com>
2021-02-27 22:53:48 +0300

    27.02.2021 commit2

---
+++ Core/Utilities/Security/JWT/ITokenhelper.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.JWT
+{
+    public interface ITokenhelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}
