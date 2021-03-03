ca1d1fc4cfe7683f8de7c9d1f1b3b9111146d254
Mehmet Can KARAYÜREK <takma_66@hotmail.com>
2021-03-04 00:13:52 +0300

    03.03.2021 Commit3DersSonu

--- Business/Concrete/ProductMenager.cs
+++ Business/Concrete/ProductMenager.cs
@@ -3,6 +3,8 @@ using Business.BussinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Entities;
@@ -32,6 +34,7 @@ namespace Business.Concrete
         }
         [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
            IResult result= BusinessRules.Run(CheckIfProductNameExists(product.ProductName), CheckIfProductCountOfCategoryCorrect(product.CategoryId),CheckIfCategoryLimitExceded());
@@ -44,10 +47,10 @@ namespace Business.Concrete
             return new SuccessResult(Messages.ProductAddedMessage);
 
         }
-        [SecuredOperation("product.list,admin")]
+        [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==23)
+            if (DateTime.Now.Hour==22)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenenceTime);
             }
@@ -59,7 +62,7 @@ namespace Business.Concrete
         {
            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
         }
-
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
@@ -79,6 +82,7 @@ namespace Business.Concrete
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
             throw new NotImplementedException();
@@ -112,6 +116,16 @@ namespace Business.Concrete
             }
             return new SuccessResult();
         }
-        
+        [TransactionScopeAspect]
+        public IResult AddTransactionalTest(Product product)
+        {
+            Add(product);
+            if (product.UnitPrice<10)
+            {
+                throw new Exception("");
+            }
+            Add(product);
+            return null;
+        }
     }
 }
