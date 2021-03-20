97e23bca411363ce6034675ca657cde1399b166b
Mehmet Can KARAYÜREK <takma_66@hotmail.com>
2021-03-20 23:54:45 +0300

    20.03.2021 Ders Sonu

--- Core/Extensions/ErrorDetails.cs
+++ Core/Extensions/ErrorDetails.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Core.Extensions;
+using FluentValidation.Results;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,10 +11,14 @@ namespace Core.Extensions
     {
         public string Message { get; set; }
         public int StatusCode { get; set; }
-
+        
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
         }
     }
+}
+public class ValidationErrorDetails : ErrorDetails
+{
+    public IEnumerable<ValidationFailure> Errors { get; set; }
 }
